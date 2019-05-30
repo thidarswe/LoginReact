ab65c51ac1717959b01c69f8440e8c9abf2f088b
Thida Swe <gnextthida@gmail.com>
2019-05-30 08:22:28 +0630

    Invoice form

--- src/App.js
+++ src/App.js
@@ -6,30 +6,120 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <center>
-        <h3>Salts</h3>
-      </center>
-        <form>
-          <div className="inputicon">
-            <i class="fa fa-user login-icon" aria-hidden="true"></i>
-            <input type="text" placeholder="admin"/>
+      <div className="row">
+        <div className="column">
+          Customer
+          <div>
+            <select className="combo_customer">
+              <option value="0">Small(1em 13px)</option>
+            </select>
           </div>
-          <span className="space"></span>
-          <div className="inputicon">
-            <i class="fa fa-key login-icon" aria-hidden="true"></i>
-            <input type="password" placeholder="......"/>
+        </div>
+        <div className="column_currency">
+          Currency
+          <div>
+            <select className="combo_currency">
+              <option value="0">JPY</option>
+            </select>
           </div>
-          <button className="login">Login</button>
-          <div className="or">
-           <i>or</i>
+        </div>
+        <div className="column_email">
+          Email<br></br>
+          <input type="text" className="email" placeholder="Small(1em 13px)"/>
+        </div>
+        <div className="column_person">
+          Person in charge
+          <input type="text" className="person" placeholder="Small(1em 13px)"/>
+        </div>
+        <div className="column">
+          Project
+          <div>
+            <select className="combo_project">
+              <option value="0">CRMotion</option>
+            </select>
           </div>
-          <button type="button" className="Login_google">
-          <div className="google">
-          <svg width="18" height="18" xmlns="http://www.w3.org/2000/svg"><g fill="#000" fill-rule="evenodd"><path d="M9 3.48c1.69 0 2.83.73 3.48 1.34l2.54-2.48C13.46.89 11.43 0 9 0 5.48 0 2.44 2.02.96 4.96l2.91 2.26C4.6 5.05 6.62 3.48 9 3.48z" fill="#EA4335"></path><path d="M17.64 9.2c0-.74-.06-1.28-.19-1.84H9v3.34h4.96c-.1.83-.64 2.08-1.84 2.92l2.84 2.2c1.7-1.57 2.68-3.88 2.68-6.62z" fill="#4285F4"></path><path d="M3.88 10.78A5.54 5.54 0 0 1 3.58 9c0-.62.11-1.22.29-1.78L.96 4.96A9.008 9.008 0 0 0 0 9c0 1.45.35 2.82.96 4.04l2.92-2.26z" fill="#FBBC05"></path><path d="M9 18c2.43 0 4.47-.8 5.96-2.18l-2.84-2.2c-.76.53-1.78.9-3.12.9-2.38 0-4.4-1.57-5.12-3.74L.97 13.04C2.45 15.98 5.48 18 9 18z" fill="#34A853"></path><path fill="none" d="M0 0h18v18H0z"></path></g></svg>
+        </div>
+      </div>
+      <div className="row">
+        <div className="column_bill">
+        Billing Address
+        <input type="text" className="bill"/>
+        </div>
+        <div className="column_term">
+        Terms
+        <div>
+          <select className="combo_terms">
+            <option value="0">Net 30</option>
+          </select>
+        </div>
+        Subject(Optional)<br></br>
+        <input type="text" className="subject" placeholder="Submect is here.."/>
+        </div>
+        <div className="column_invoice">
+        Invoice date<br></br>
+        <input type="text" className="date" placeholder="01-Feb-2019"/>
+        <i class="fa fa-calendar" aria-hidden="true"></i>
+        </div>
+        <div className="column_due">
+        Due date<br></br>
+        <input type="text" className="date" placeholder="01-Feb-2019"/>
+        <i class="fa fa-calendar" aria-hidden="true"></i>
+        </div>
+      </div>
+      <div>
+        <table className="table_product">
+          <tr>
+            <th className="No"></th>
+            <th className="product">Product/Service</th>
+            <th className="discription">Discription</th>
+            <th className="price">Price</th>
+            <th className="qty">Qty</th>
+            <th className="tax">Tax</th>
+            <th className="amount">Amount</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>CRMotion</td>
+            <td>May 2019 labofee</td>
+            <td className="td_price">JPY 1,200,000</td>
+            <td className="td_qty">1</td>
+            <td></td>
+            <td className="td_amount">JPY 1,200,000</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td colspan="6"></td>
+          </tr>
+        </table>
+        <div className="row">
+          <div className="column_note">
+            <button type="text" className="btn_action">Add lines</button><br/>
+            Note<br/>
+            <input type="text" className="note"/>
           </div>
-          <span>Login with Google</span></button>
-          <FontAwesomeIcon icon={['fab', 'google']} />
-        </form>
+          <div className="column_total">
+            <table className="table_total">
+              <tr>
+                <td>Total</td><td>1,200,000</td>
+              </tr>
+              <tr>
+                <td>Discount</td><td>10,000</td>
+              </tr>
+              <tr>
+                <td>Taxabel</td><td>1,200,000</td>
+              </tr>
+              <tr>
+                <td>Tax</td><td>60,000</td>
+              </tr>
+              <tr>
+              <td>Ground Total</td>
+              <td>JPY1,260,000</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+      <button type="button" className="btn_save">Save</button>
     </div>
   );
 }
